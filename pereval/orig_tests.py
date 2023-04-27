85a97ea11089792640555495abcfe76656fb8176
ReutAS <reutas39@gmail.com>
2023-04-27 15:16:20 +0200

    tests modified

--- pereval/orig_tests.py
+++ pereval/orig_tests.py
@@ -10,6 +10,17 @@ from .serializers import PerevalSerializer
 
 client = Client()
 
+
+# class PerevalAddedTest():
+#     '''тест модели'''
+#     def setUp(self):
+#         Level.objects.create(winter='1А', summer='1А', autumn='1Б',spting='1Б')
+#         Coords.objects.create(latitude=33.3, longitude=55.5, height=777)
+#         Users.objects.create(email='test@test.ru', phone='+79521542323', name='Тест', fam='Тестов', otc='Тестович')
+#         Images.objects.create(title='Тест заголовок', data='https://test.ru/uploads/2020/08/test-36.jpg')
+#         #PerevalAdded.objects.create()
+
+
 class CreateNewPerevalTest(APITestCase):
     """ Test module for inserting a new pereval """
 
@@ -169,7 +180,7 @@ class GetPatchSinglePerevalTest(TestCase):
         serializer = PerevalSerializer(pereval)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+        print(PerevalAdded.objects.all())
     def test_get_invalid_single_pereval(self):
         response = client.get(
             reverse('submitData-detail', kwargs={'pk': 30}))
@@ -190,3 +201,90 @@ class GetPatchSinglePerevalTest(TestCase):
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+
+# class UpdateSinglePerevalTest(APITestCase):
+#     """ Test module for updating an existing puppy record """
+#
+#     def setUp(self):
+#         self.level = Level.objects.create(winter='1А', summer='1А', autumn='1Б', spring='1Б')
+#         self.coords = Coords.objects.create(latitude=33.3, longitude=55.5, height=777)
+#         self.user = Users.objects.create(email='test@test.ru', phone='+79521542323', name='Тест', fam='Тестов', otc='Тестович')
+#         self.image = Images.objects.create(title='Тест заголовок', data='https://test.ru/uploads/2020/08/test-36.jpg')
+#         self.pereval = PerevalAdded.objects.create(user=self.user, coords=self.coords, level=self.level)
+#         self.perevalimages = PerevaladdedImages.objects.create(images=self.image, perevaladded=self.pereval)
+#         self.valid_payload = {
+#             "user": {
+#                 "email": "test@example.com",
+#                 "phone": "+79226120668",
+#                 "fam": "Пупкин",
+#                 "name": "Васиилий",
+#                 "otc": "Иванович"
+#             },
+#             "coords": {
+#                 "latitude": 43.17257,
+#                 "longitude": 42.80231,
+#                 "height": 3710
+#             },
+#             "level": {
+#                 "winter": "2А",
+#                 "summer": "1Б",
+#                 "autumn": "1Б",
+#                 "spring": "1Б"
+#             },
+#             "images": [
+#                 {
+#                     "data": "http://127.0.0.1:8000/photos/2023/04/11/19._%D0%A1%D0%B0%D0%BC%D0%BE%D0%BB%D0%B5%D1%82%D1%8B.jpg",
+#                     "title": "Вид с вершины"
+#                 }
+#             ],
+#             "beauty_title": "пер.",
+#             "title": "Гарвашииии",
+#             "other_titles": "",
+#             "connect": ""
+#         }
+#
+#         self.invalid_payload = {
+#             'user': {
+#                 'name': 12,
+#                 'fam': 'Test Fam',
+#                 'otc': 'Test Otc',
+#                 'email': '',
+#                 'phone': '+73521452528'
+#             },
+#             'coords': {
+#                 'latitude': 123,
+#                 'longitude': 678.9,
+#                 'height': 777,
+#             },
+#             'level': {
+#                 'winter': '1А',
+#                 'summer': 'н/к',
+#                 'autumn': 'н/к',
+#                 'spring': 'Tн/к'
+#             },
+#             'images': [
+#                 {
+#                     'title': 'Test Image',
+#                     'img': 'test.jpg'
+#                 }
+#             ],
+#             "beauty_title": "пер.",
+#             "title": "Гарваш",
+#             "other_titles": "",
+#             "connect": ""
+#         }
+#
+#     def test_valid_update_pereval(self):
+#         response = client.patch(
+#             reverse('submitData-detail', kwargs={'pk': self.pereval.pk}),
+#             data=json.dumps(self.valid_payload),
+#             content_type='application/json'
+#         )
+#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+#
+#     def test_invalid_update_pereval(self):
+#         response = client.patch(
+#             reverse('submitData-detail', kwargs={'pk': self.pereval.pk}),
+#             data=json.dumps(self.invalid_payload),
+#             content_type='application/json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
